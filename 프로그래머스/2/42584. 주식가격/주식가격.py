bf1ef8b6477df050304080809fb9d2b068647e38
Kang Hye Ri <121994108+merrykang@users.noreply.github.com>
2024-06-20 06:26:11 +0900

    [level 2] Title: 주식가격, Time: 14.96 ms, Memory: 16.9 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.py
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -1,24 +1,17 @@
 def solution(prices):
-    stack = []
-    answer = [0] * len(prices)
-    for i in range(len(prices)):
-        while stack and prices[i] < prices[stack[-1]]:
-            j = stack.pop()
-            answer[j] = i - j
-        stack.append(i)
-    while stack:
-        j = stack.pop()
-        answer[j] = len(prices) - 1 - j
-    return answer
-
-# def solution(prices):
-#     ans = [0] * len(prices)
-#     for i in range(len(prices)):
-#         if i == len(prices) - 1:
-#             # ans[i] -= 1
-#             return ans
-#         for j in range(len(prices)):
-#             if i >= j: continue
-#             ans[i] += 1
-#             if prices[i] > prices[j]: break
-#             # else: ans[i] += 1 
+    t = [0] * len(prices)
+    s = []
+    # 인덱스를 append 하는 생각 못 함
+    # 가격이 떨어지는 경우 / 마지막까지 떨어지지 않은 경우로 나누어서 생각할 생각 하지 못 함
+        # 마지막에 t 만 처리할 수 없나: 전부 ? 일부? -> 그 이유: 마지막까지 떨어지지 않은 경우임
+        # 가격이 떨어지는 경우(핵심 로직): 스택의 pop 인덱스의 prices 가 이전 인덱스의 첫 번째 가격 떨어짐이 될 수 있다
+        # 가격이 떨어지지 않은 경우(핵심 로직): s가 있을 때까지
+    for i in range(len(prices)):  # 결국 반복문 2번 돌리긴 함
+        while s and prices[s[-1]] > prices[i] :  # while인 이유 = 반복문 + 조건문(현재 pop 인덱스의 prices가 i 인덱스의 첫 번째 가격 떨어짐이었는데, i-1 i-2 의 경우에도 첫 번째 가격 떨어짐일 수 있음) / list index out of range 고려하여 s 있는지 여부 따져야함
+            j = s.pop()
+            t[j] = i - j
+        s.append(i)        
+    while s:
+        k = s.pop()
+        t[k] = len(prices) - k - 1
+    return t
