0906e128af8b68a732ad54a2db6df700c862ef7b
Kang Hye Ri <merrykang1103@gmail.com>
2025-08-03 09:37:42 +0900

    250803 - 백준 2231 브론즈2 분해합

---
+++ new/250803-백준 2231 브론즈2 분해합.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int n = Integer.parseInt(br.readLine());
+    int[] arr = new int[n + 1];
+    for (int i = 1; i < arr.length; i++)
+      arr[i] = i;
+
+    // n의 생성자 리스트 구하기
+    // 현재 숫자의 각 자리 수의 합을 구해야 하므로 String으로 변환하여 각 자리를 split
+    List<Integer> constructor = new ArrayList<>();
+    for (int i = 1; i < arr.length; i++) {
+      int sum = i;
+      String numToString = String.valueOf(arr[i]);
+      String[] strArr = numToString.split("");
+      for (int j = 0; j < strArr.length; j++)
+        sum += Integer.parseInt(strArr[j]);
+      if (sum == n)
+        constructor.add(i);
+    }
+
+    // 정답 출력
+    // 가장 작은 값을 출력하기 위해 정렬 후 첫 번째 원소 출력
+    // 예외 처리 하지 않으면 indexOutOfBounds 에러 출력됨
+    Collections.sort(constructor);
+    System.out.println(constructor.size() > 0 ? constructor.get(0) : 0);
+  }
+}
